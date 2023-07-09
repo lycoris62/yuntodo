9823d16bfd402a6d0d2fe1dce29d05f2143110db
barami62 <wodbs7758@naver.com>
2023-07-10 03:27:07 +0900

    first commit

---
+++ src/main/java/me/yuntodo/YuntodoApplication.java
@@ -0,0 +1,13 @@
+package me.yuntodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuntodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuntodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/yuntodo/YuntodoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yuntodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuntodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
