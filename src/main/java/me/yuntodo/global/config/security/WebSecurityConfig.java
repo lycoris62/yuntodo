b7159dd1e5cb92e9547909b89a810c3d81b719f2
barami62 <wodbs7758@naver.com>
2023-07-11 00:59:43 +0900

    fix : H2 console 허용

--- src/main/java/me/yuntodo/global/config/security/WebSecurityConfig.java
+++ src/main/java/me/yuntodo/global/config/security/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,10 +29,14 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console())
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
 
-        http.authorizeHttpRequests(a -> a.requestMatchers(PathRequest.toH2Console()).permitAll());
-        http.authorizeHttpRequests(a -> a.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll());
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.csrf(AbstractHttpConfigurer::disable);
         http.cors(AbstractHttpConfigurer::disable);
