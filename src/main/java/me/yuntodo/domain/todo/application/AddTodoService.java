6841a002edc5e91997ea8d6d5df4bb8225c53b61
barami62 <wodbs7758@naver.com>
2023-08-20 18:21:05 +0900

    feat : 다른 유저의 투두 조회 막기

--- src/main/java/me/yuntodo/domain/todo/application/AddTodoService.java
+++ src/main/java/me/yuntodo/domain/todo/application/AddTodoService.java
@@ -6,6 +6,7 @@ import me.yuntodo.domain.todo.dao.TodoRepository;
 import me.yuntodo.domain.todo.domain.Todo;
 import me.yuntodo.domain.todo.dto.AddTodoRequest;
 import me.yuntodo.domain.todo.dto.TodoResponse;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -15,6 +16,7 @@ public class AddTodoService {
 
     private final TodoRepository todoRepository;
 
+    @PreAuthorize("#username == authentication.principal.username")
     public TodoResponse addTodo(AddTodoRequest request, String username) {
         Todo todo = new Todo(request.getContent(), username);
         Todo savedTodo = todoRepository.save(todo);
--- src/main/java/me/yuntodo/domain/todo/dao/TodoRepository.java
+++ src/main/java/me/yuntodo/domain/todo/dao/TodoRepository.java
@@ -2,10 +2,17 @@ package me.yuntodo.domain.todo.dao;
 
 import me.yuntodo.domain.todo.domain.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.access.prepost.PostFilter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+    @PostFilter("filterObject.username == authentication.name")
     List<Todo> findAllByUsernameAndCreatedAtBetween(String username, LocalDateTime start, LocalDateTime end);
+
+    @Query("SELECT t FROM Todo t WHERE t.username=?#{authentication.name}")
+    List<Todo> findAllByUsername(String username);
 }
